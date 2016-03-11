9b75bd54fb005454293f0095835ad32a39865513
Centimitr <Centimitr@163.com>
2016-03-11 15:02:20 +0800

    basic structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -17,49 +17,66 @@ import {Home} from './home/home';
   providers: [ ...FORM_PROVIDERS ],
   directives: [ ...ROUTER_DIRECTIVES, RouterActive ],
   pipes: [],
-  styles: [`
-    nav ul {
-      display: inline;
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      width: 60px;
-    }
-    nav li {
-      display: inline;
-    }
-    nav li.active {
-      background-color: lightgray;
-    }
-  `],
-  template: `
-    <header>
-      <nav>
-        <h1>Hello {{ name }}</h1>
-        <ul>
-          <li router-active>
-            <a [routerLink]=" ['Index'] ">Index</a>
-          </li>
-          <li router-active>
-            <a [routerLink]=" ['Home'] ">Home</a>
-          </li>
-          <li router-active>
-            <a [routerLink]=" ['About'] ">About</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+  //styles: [`
+  //  nav ul {
+  //    display: inline;
+  //    list-style-type: none;
+  //    margin: 0;
+  //    padding: 0;
+  //    width: 60px;
+  //  }
+  //  nav li {
+  //    display: inline;
+  //  }
+  //  nav li.active {
+  //    background-color: lightgray;
+  //  }
+  //`],
+  template: `<header>
+  <nav class="navbar navbar-light bg-faded">
+  <div class="nav navbar-nav container">
+      <a class="nav-item nav-link" [routerLink]=" ['About'] ">About</a>
+    <a class="nav-item nav-link" [routerLink]=" ['Index'] ">Index</a>
+    <a class="nav-item nav-link" [routerLink]=" ['Home'] ">Home</a>
+    </div>
+  </nav>
+</header>
 
-    <main>
-      <router-outlet></router-outlet>
-    </main>
+<main>
+<br><br>
+<section class="container">
+<h4>Init</h4>
+<button class="btn btn-secondary">Set source lang</button>
+<button class="btn btn-secondary">Set target lang</button>
+<hr>
+<h4>Versions</h4>
+<button class="btn btn-secondary">Add Version</button>
+<br>
+alpha.0 18456/45613 45%<br>
+alpha.1 <br>
+alpha.2 <br>
+beta.0 <br>
+<hr>
+<h4>Watch list</h4>
+<button class="btn btn-secondary">change base version</button>
+<button class="btn btn-secondary">Init Watch</button>
+<button class="btn btn-secondary">Init translate</button>
+<button class="btn btn-secondary">Add Watch</button>
+<br>
+select version <br>
+-blabla <br>
+---blabla 12345/4567 30% complete<br>
+<hr>
+<h4>Comparison</h4>
+<div>README.md</div>
+<div>compare: 151+ 100-</div>
+<div>progress: 123/251</div>
+</section>
+  <router-outlet></router-outlet>
+</main>
 
-    <footer>
-      WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a>
-      <div>
-        <img [src]="angularclassLogo" width="10%">
-      </div>
-    </footer>
+<footer>
+</footer>
   `
 })
 @RouteConfig([
